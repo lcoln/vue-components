f9db933d039b07e5d0ac274732153d23c157b5de
linteng <875482941@qq.com>
2017-11-21 20:40:18 +0800

    完善全屏滚动组件es5, es6写法

---
+++ src/components/fullscreen-scroll/main-es5.js
@@ -0,0 +1,169 @@
+/**
+ *
+ * @authors linteng (875482941@qq.com)
+ * @date    2017-09-07 19:53:18
+ */
+'use strict'
+
+var pageNums = 0
+var listen = function(el, ev, fn, capture){
+    if(window.addEventListener){
+        el.addEventListener(ev, fn, capture)
+    }else if(window.attachEvent){
+        el.attachEvent('on' + ev, fn, capture)
+    }else{
+        el['on' + ev] = fn
+    }
+}
+
+var remove = function(el, ev, fn, capture){
+    if(window.removeEventListener){
+        el.removeEventListener(ev, fn, capture)
+    }else if(window.attachEvent){
+        el.attachEvent('on' + ev, fn, capture)
+    }
+}
+
+var watch = function(obj, prop, cb){
+    var oldVal = ''
+    if(prop in obj){
+        oldVal = obj[prop]
+    }
+    Object.defineProperty(obj, prop, {
+        get: function(){
+            return oldVal
+        },
+        set: function(newVal){
+            oldVal = newVal
+            cb && cb(newVal)
+        }
+    })
+}
+
+/**
+ * [监听对象所有属性变化]
+ * @param  {Object}   obj [要监听属性的对象]
+ * @param  {Function} cb  [description]
+ */
+var observable = function(obj, prop, cb){
+    var type = Object.prototype.toString.call(prop)
+    var props = []
+    if(type === '[object String]'){
+        props = [prop]
+    }else if(type === '[object Function]'){
+        props = Object.keys(obj)
+        cb = prop
+    }else if(type === '[object Array]'){
+        props = prop
+    }
+    for(var i = 0;i < props.length;i++){
+        watch(obj, props[i], cb)
+    }
+
+}
+
+window.Scroll = function (parendId, childId, time){
+    this.id = Date.now() + '_scroll'
+    this.curPage = 0
+    this.init = function(parendId, childId, time){
+        this.el = this.$el(parendId, childId, time)
+        var container = this.el.querySelector('.' + childId).parentNode,
+        childDom = container.childNodes
+
+        this.curHeight = (window.getComputedStyle ? (window.getComputedStyle(this.el).height).replace('px', '') : this.el.offsetHeight) >> 0
+
+        var mouseWheelEv = /Firefox/i.test(navigator.userAgent) ? "DOMMouseScroll": "mousewheel",
+        direction = /Firefox/i.test(navigator.userAgent) ? "detail": "deltaY",
+        now = Date.now()
+        var _this = this
+        listen(this.el, mouseWheelEv, function(ev){
+            if(Date.now() - now > 1000){
+                if(ev[direction] > 0){
+                    if(_this.curPage + 1 < childDom.length){
+                        _this.curPage++
+                    }else{
+                        _this.curPage = 0
+                    }
+                }else{
+                    if(_this.curPage - 1 >= 0){
+                        _this.curPage--
+                    }else{
+                        _this.curPage = childDom.length - 1
+                    }
+
+                }
+                container.style.transform = 'translate(0px, ' + -_this.curHeight * _this.curPage + 'px)'
+                now = Date.now()
+            }
+        })
+
+        listen(window, 'resize', function(){
+
+            _this.curHeight = (window.getComputedStyle ? (window.getComputedStyle(_this.el).height).replace('px', '') : _this.el.offsetHeight) >> 0
+            container.style.transition = '0s'
+            container.style.transform = 'translate(0px, ' + -_this.curHeight * _this.curPage + 'px)'
+            setTimeout(function(){
+                container.style.transition = time.indexOf('s') < 0 ? time + 's' : time
+            }, 0)
+        })
+
+        observable(this, 'curPage', function(index){
+            var btnGroup = document.getElementById('ui-fullpage-btn-' + _this.id),
+                span = btnGroup.childNodes
+            for(var i = 0;i < span.length;i++){
+                if(i == index){
+                    span[index].style.background = '#f30'
+                    span[index].style.transform = 'scale(1.5)'
+                }else{
+                    span[i].style.background = 'rgba(255,255,255,.6)'
+                    span[i].style.transform = ''
+                }
+            }
+
+        })
+    }
+}
+
+Scroll.prototype.$el = function(parendId, childId, time){
+    var parentDom = document.getElementById(parendId)
+    parentDom.style.overflow = 'hidden'
+
+    var childDom = parentDom.querySelectorAll('.' + childId)
+    var container = document.createElement('section')
+    var btnGroup = document.createElement('section')
+
+    var span = document.createElement('span')
+    var frag = document.createDocumentFragment()
+    var transition = time.indexOf('s') < 0 ? time + 's' : time
+    var _this = this
+    for(var i = 0;i < childDom.length;i++){
+        container.appendChild(childDom[i])
+        var btn = span.cloneNode(false)
+        btn.setAttribute('style', 'display: inline-block;width: 10px;height: 10px;border-radius: 100%;background: rgba(255,255,255,.6);transition: .4s;')
+        if(i == 0){
+            btn.style.background = '#f30'
+            btn.style.transform = 'scale(1.5)'
+        }
+        ;(function(i){
+            listen(btn, 'click', function(){
+                _this.curPage = i
+                container.style.transform = 'translate(0px, ' + -_this.curHeight * _this.curPage + 'px)'
+            })
+        })(i);
+        frag.appendChild(btn)
+    }
+    btnGroup.appendChild(frag)
+    btnGroup.setAttribute('style', 'position: absolute;right: 10px;top: 50%;width: 10px;text-align: center;transform: translate(0, -50%);transition: '+ transition +' ')
+    btnGroup.id = 'ui-fullpage-btn-' + this.id
+
+    container.style.width = '100%'
+    container.style.height = '100%'
+    container.style.transition = time.indexOf('s') < 0 ? time + 's' : time
+    container.id = 'pageName-' + this.id
+
+
+    parentDom.style.position = 'relative'
+    parentDom.appendChild(container)
+    parentDom.appendChild(btnGroup)
+    return parentDom
+}
--- src/template/index/fullscreen_scroll.vue
+++ src/template/index/fullscreen_scroll.vue
@@ -5,24 +5,27 @@
             <a class="tab" href="javascript:;" :class="{'act': showType == 2}" @click="showType = 2">代码</a>
         </section>
         <section class="container">
-            <div class="exam" v-if="showType == 1">
-                <fullpage :amount="5" :bg-color="bgColor" :cur-page="curIndex" @on-curIndex-change="changeCurIndex">
-                    <div slot="page1" class="page">111111</div>
-                    <div slot="page2" class="page">222222</div>
-                    <div slot="page3" class="page">333333</div>
-                    <div slot="page4" class="page">444444</div>
-                    <div slot="page5" class="page">555555</div>
-                    <!-- <template slot="ui-fullpage-tab">
-                        <div class="website-index-tab-group">
-                            <span v-for="(page, index) in tabText" :class="{'tab-act': curIndex == index}" class="website-index-tab" @click="go(index >> 0)">
-                                {{page}}
-                                <i class="tab-line" :class="{'tab-line-act': curIndex == index}"></i>
-                            </span>
-                        </div>
-                    </template> -->
-                </fullpage>
+            <div class="exam" v-show="showType == 1">
+                <div id="fullPage">
+                    <div class="page"></div>
+                    <div class="page"></div>
+                    <div class="page"></div>
+                    <div class="page"></div>
+                    <div class="page"></div>
+                </div>
+                <div id="fullPage2">
+                    <div class="page"></div>
+                    <div class="page"></div>
+                    <div class="page"></div>
+                    <div class="page"></div>
+                    <div class="page"></div>
+                </div>
             </div>
-            <pre class="code" v-if="showType == 2">2222</pre>
+            <pre class="code" v-if="showType == 2" v-text="code">
+                <code class="html">
+
+                </code>
+            </pre>
         </section>
     </div>
 </template>
@@ -30,14 +33,35 @@
 <script>
 
     import vue from 'vue'
-    import fullpage from '@/components/fullscreen-scroll/main.vue'
+    import fullpage from '@/components/fullscreen-scroll/main-es6.js'
     export default {
         data () {
             return {
                 bgColor: ['#182327', '#202834', '#2b3958', '#1B1521', '#2D363F'],
                 curIndex: 0,
                 curArr: [0],
-                showType: 1
+                showType: 1,
+                code: `
+<div id="fullPage">
+    <div class="page"></div>
+    <div class="page"></div>
+    <div class="page"></div>
+    <div class="page"></div>
+    <div class="page"></div>
+</div>
+<div id="fullPage2">
+    <div class="page"></div>
+    <div class="page"></div>
+    <div class="page"></div>
+    <div class="page"></div>
+    <div class="page"></div>
+</div>
+<script>
+    var fullpage = new fullpage()
+    fullpage.init('fullPage', 'page', '.4')
+    var fullpage2 = new fullpage()
+    fullpage2.init('fullPage2', 'page', '.4')
+<\/script>`
             }
         },
         methods: {
@@ -52,24 +76,47 @@
             }
         },
         mounted: function(){
-            vue.log(fullpage)
+            require('@/components/fullscreen-scroll/main-es5.js')
+            var fullpage1 = new Scroll()
+            fullpage1.init('fullPage', 'page', '.4')
+            var fullpage2 = new Scroll()
+            fullpage2.init('fullPage2', 'page', '.4')
+            console.log(fullpage2);
         },
-        components: {
+        /*components: {
             fullpage
-        }
+        }*/
     }
 </script>
 
 <style rel="stylesheet" lang="scss" type="text/css" scoped>
+
+    #fullPage{width: 100%;height: 500px;box-shadow: 0 0 5px #333;background: #ddd;}
+    #fullPage .page{width: 100%;height: 100%;background: #bbb;}
+    #fullPage .page:nth-of-type(1){background: #182327}
+    #fullPage .page:nth-of-type(2){background: #132C33}
+    #fullPage .page:nth-of-type(3){background: #F9F9F9}
+    #fullPage .page:nth-of-type(4){background: #015E7F}
+    #fullPage .page:nth-of-type(5){background: #2D363F}
+
+    #fullPage2{width: 100%;height: 620px;margin: 10px 0;box-shadow: 0 0 5px #333;background: #ddd;}
+    #fullPage2 .page{width: 100%;height: 100%;background: #bbb;}
+    #fullPage2 .page:nth-of-type(1){background: #182327}
+    #fullPage2 .page:nth-of-type(2){background: #132C33}
+    #fullPage2 .page:nth-of-type(3){background: #F9F9F9}
+    #fullPage2 .page:nth-of-type(4){background: #015E7F}
+    #fullPage2 .page:nth-of-type(5){background: #2D363F}
+
+    pre{tab-size: 4;}
     .content{width: 100%;height: 100%;
         .tab-group{float: left;width: 100%;border-bottom: 1px solid #ddd;
             .tab{display: inline-block;width: 100px;height: 40px;margin-bottom: -1px;line-height: 40px;text-align: center;transition: .4s;color: #333;
                 &.act{border-bottom: 2px solid #182445;}
             }
         }
-        .container{width: 100%;padding-top: 40px;
-            .code{height: 100%;margin: 30px;padding: 10px;box-shadow: 0 0 5px #ddd;background: #F7F7F7;}
-            .exam{height: 500px;margin: 30px;box-shadow: 0 0 5px #333;}
+        .container{width: 100%;overflow: auto;
+            .code{height: 100%;margin: 30px;padding: 10px;box-shadow: 0 0 5px #ddd;background: #F7F7F7;overflow: auto;}
+            .exam{margin: 30px;}
         }
     }
 </style>
