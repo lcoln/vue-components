adeeb4b17a29c8529ff79fc2f30698241b25ed84
linteng <875482941@qq.com>
2017-12-17 21:40:32 +0800

    优化瀑布组件

--- src/components/waterfall/main-es5.js
+++ src/components/waterfall/main-es5.js
@@ -76,9 +76,11 @@ UiWaterFall.prototype.init = function(parendId, childId, config){
     var parentDom = document.getElementById(parendId),
         childDom = parentDom.querySelectorAll('.' + childId)
 
+    if(!UiWaterFall.prototype.hasCss){
+        document.head.innerHTML += '<link rel="stylesheet" type="text/css" href="/static/css/waterfall/waterfall.css">'
+    }
+    UiWaterFall.prototype.hasCss = true
 
-    var container = document.createElement('section')
-    container.className = 'ui-waterfall'
     config = config || {}
     this.config = {
         margin: config.margin || 5,
@@ -96,29 +98,20 @@ UiWaterFall.prototype.init = function(parendId, childId, config){
         frag.appendChild(childDom[i])
     }
 
+    var container = document.createElement('section')
+    container.className = 'ui-waterfall'
     container.appendChild(frag)
-    var containerw = parentDom.offsetWidth
-    var boxMarginLeft = childDom[0].style.marginLeft.replace('px', '') >> 0
-    var boxMarginRight = childDom[0].style.marginRight.replace('px', '') >> 0
-    var boxw = (childDom[0].style.width.slice(0, -2) >> 0) + boxMarginLeft + boxMarginRight
-    var maxColumLen = Math.floor(containerw / boxw)
 
-    if(!UiWaterFall.prototype.hasCss){
-        document.head.innerHTML += '<link rel="stylesheet" type="text/css" href="/static/css/waterfall/waterfall.css">'
-    }
-    UiWaterFall.prototype.hasCss = true
+    this.setContainerWidth(parentDom, childDom, container).setPosition(parentDom, childDom)
 
-    var w = maxColumLen * (width + margin * 2)
-    container.style.cssText = 'width: ' + w + 'px;'
     parentDom.appendChild(container)
-    this.setPosition(parentDom, childDom)
 
     var _this = this
     listenEvent(window, 'resize', function(){
         var parentDom = document.getElementById(parendId),
             childDom = parentDom.querySelectorAll('.' + childId)
 
-        _this.setPosition(parentDom, childDom)
+        _this.setContainerWidth(parentDom, childDom, container).setPosition(parentDom, childDom)
     })
 }
 
@@ -126,43 +119,41 @@ UiWaterFall.prototype.setPosition = function(container, box){
     if(!container || !box)
         return
     var margin = this.config.margin
-    var width = this.config.width
 
-    let containerw = container.offsetWidth
-    let boxMarginLeft = box[0].style.marginLeft.replace('px', '') >> 0
-    let boxMarginRight = box[0].style.marginRight.replace('px', '') >> 0
-    let boxMarginTop = box[0].style.marginTop.replace('px', '') >> 0
-    let boxMarginBottom = box[0].style.marginBottom.replace('px', '') >> 0
+    var column = []
+    for(var i = 0;i < box.length;i++){
 
-    let boxw = box[0].offsetWidth + boxMarginLeft + boxMarginRight
-    let maxColumLen = Math.floor(containerw / boxw)
+        var top = 0
+        var left = this.boxw * (i % this.maxColumLen) + 'px'
 
-    var w = maxColumLen * (width + margin * 2)
-    container.querySelector('.ui-waterfall').style.cssText = 'width: ' + w + 'px;'
+        var boxh = parseFloat(box[i].style.height.slice(0, -2))
 
-
-    let column = []
-    for(let i = 0;i < box.length;i++){
-        let margin = boxMarginTop + boxMarginBottom
-
-        let top = 0
-        let left = boxw * (i % maxColumLen) + 'px'
-
-        let boxh = parseFloat(box[i].style.height.slice(0, -2))
-
-        if(column.length < maxColumLen){
+        if(column.length < this.maxColumLen){
             column.push(boxh)
         }else{
-            let tmpColumn = JSON.parse(JSON.stringify(column))
-            let index = tmpColumn.indexOf(column.sort(function(a, b){return a - b})[0])
+            var tmpColumn = JSON.parse(JSON.stringify(column))
+            var index = tmpColumn.indexOf(column.sort(function(a, b){return a - b})[0])
             tmpColumn[index] += boxh + margin
-            left = boxw * index + 'px'
+            left = this.boxw * index + 'px'
             top = column[0] + margin + 'px'
             column = tmpColumn
         }
 
         box[i].style.cssText += `transform: translate(${left},${top});text-align: center;`
     }
+}
+
+UiWaterFall.prototype.setContainerWidth = function(parentDom, childDom, container){
+    var containerw = parentDom.offsetWidth
+    var boxMarginLeft = childDom[0].style.marginLeft.replace('px', '') >> 0
+    var boxMarginRight = childDom[0].style.marginRight.replace('px', '') >> 0
+    this.boxw = (childDom[0].style.width.slice(0, -2) >> 0) + boxMarginLeft + boxMarginRight
+    this.maxColumLen = Math.floor(containerw / this.boxw)
+
+
+    var w = this.maxColumLen * (this.config.width + this.config.margin * 2)
+    container.style.cssText = 'width: ' + w + 'px;'
+    return this
 }
 
 UiWaterFall.prototype.hasCss = false
--- src/template/index/waterfall.vue
+++ src/template/index/waterfall.vue
@@ -79,7 +79,7 @@ es6: https://components.cncoders.me/components/waterfall/main-es6.js //import Ui
         mounted: async function(){
             require('@/components/waterfall/main-es5.js')
             var waterfall = new UiWaterFall()
-            // console.log(waterfall);
+            console.log(waterfall);
             waterfall.init('waterfall', 'waterfall-box')
             /*var waterfall1 = new UiWaterFall()
             waterfall1.init('waterfall1', 'waterfall-box', {width: 200, range: [300, 400]})*/
